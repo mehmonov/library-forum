b1223a8ac331bb6234de3ad7e71e045a055e10fe
Husniddin <uzbekfreedom@gmail.com>
2024-05-25 16:33:03 +0500

    login and register completed

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import user_login
+from .views import user_login, user_logout, user_register, user_profile
 urlpatterns = [
-    path('', user_login, name='user_login'),
-  
+    path('', user_login.as_view(), name='user_login'),
+    path('user_register', user_register.as_view(), name='user_register'),
+    path('user_profile', user_profile, name='user_profile'),
+    
+    path('user_logout',user_logout, name='user_logout' )
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+user = get_user_model()
 
-# Create your views here.
+class user_login(View):
+    def get(self, request):
+        return render(request, 'accaunt/login.html')
+    
+    def post(self, request):
+        username  = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+           login(request, user)
+           messages.success(request, "Login .")
+           return redirect('home_page')
+        else:
+            messages.error(request, "Login qilinmadi")
+            return render(request, 'accaunt/login.html')
 
 
-def user_login(request):
-    return  render(request, 'accaunt/login.html')
+class user_register(View):
+    def get(self, request):
+        return render(request, 'accaunt/login.html')
+    def post(self, request):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username  = request.POST.get('username')
+        password = request.POST.get('password')
+        repeatPassword = request.POST.get('repeatPassword')
+        
+        if password == repeatPassword:
+            
+            try:
+                user.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=make_password(password)
+            )
+            except Exception as e:
+                print(f'xato --- {e}')
+                
+                
+            newUser = authenticate(username=username, password=password)
+            print(newUser)
+            if newUser is not None:
+                login(request, newUser)
+               
+                return redirect('home_page')
+            else:
+                messages.error(request, "ro'yhatdan o'tmadi")
+                return render(request, 'accaunt/login.html')
+
+        return redirect('home_page')
+def user_logout(request):
+    logout(request)
+    
+    return redirect('user_login')
+
+
+def user_profile(request):
+    pass
